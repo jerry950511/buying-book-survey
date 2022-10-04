50ff85dad31a02f1a0453172dc754169cb297ed4
嘎嘎 <j0970238552@gmail.com>
2022-10-05 00:38:12 +0800

    1005

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash , jsonify
+from flask import Flask, render_template, request, redirect , jsonify
 import eportal
 app = Flask(__name__)
 
@@ -16,12 +16,12 @@ def login():
 
 @app.route("/eportal", methods=['POST'])
 def eportal_login():
-    account = request.form.get['account']
-    password = request.form.get['password']
+    account = request.form.get('account')
+    password = request.form.get('password')
     if eportal.check_eportal(account,password):
         return jsonify({"message":"登入成功"})
     else:
         return jsonify({"message":"帳號或密碼錯誤"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
