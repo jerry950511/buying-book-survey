221be97d694f02e985d28411ea76afa2f45527a7
csiejar <j0970238552@gmail.com>
2022-11-27 15:57:56 +0800

    run server

--- main.py
+++ main.py
@@ -28,5 +28,6 @@ def get_info():
     password = request.args.get('password')
     info = eportal.get_info(account,password)
     return jsonify({"data":info})
-if __name__ == '__main__':
-    app.run(debug=True)
+#run server
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
